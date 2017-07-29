2ac1426ae44dad848bec5d8b1b71d93f9d50ba15
repraze <repraze@gmail.com>
2017-07-29 01:42:44 +0100

    Fixes web console newline logic, adds line width

--- client/public/js/console.js
+++ client/public/js/console.js
@@ -2,7 +2,8 @@
     const isMobile = ('ontouchstart' in document.documentElement);
     $.fn.mmud = function(settings, client){
         settings = Object.assign({
-            max : 100
+            max : 100,
+            lineWidth : 80
         }, settings);
         client = client || new Client();
 
@@ -27,28 +28,45 @@
 
             var last;
             var count = 0;
+            var linelength = 0;
             var newLine = function(){
                 last = $('<div class="mmud-line"></div>');
                 text.append(last);
-                ++count;
-                if(count<settings.max){
+                if(count<=settings.max){
                     ++count;
                 }else{
                     text.find('div').first().remove();
                 }
+                linelength = 0;
                 return last;
             };
             newLine();
+            var addStr = function(str){
+                let available = settings.lineWidth-linelength;
+                if(str.length < available){
+                    linelength+=str.length;
+                    last.append(str);
+                }else{
+                    linelength+=available;
+                    last.append(str.substring(0,available));
+                    newLine();
+                    addStr(str.substring(available,str.length));
+                }
+            };
 
             var add = function(str){
                 var shouldScroll = sroll[0].scrollHeight===sroll.scrollTop()+sroll[0].offsetHeight;
 
                 if(str.indexOf("\n")==-1){
                     last.append(str);
+                    addStr(str);
                 }else{
+                    console.log(str.split(/\r?\n/));
                     str.split(/\r?\n/).forEach(function(line){
-                        last.append(line);
-                        newLine();
+                        addStr(line);
+                        if(line !== ""){
+                            newLine();
+                        }
                     });
                 }
 
